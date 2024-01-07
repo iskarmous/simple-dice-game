bcfb57b62e0ec2ca5110cf481783260cdda3b779
iskarmous <84739456+iskarmous@users.noreply.github.com>
2024-01-07 16:43:26 +0100

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -1,57 +1,95 @@
-function tiraDadi() {
+let risultatiCPU = [];
+let risultatiPlayer = [];
 
-        document.getElementById("dadiCPU").innerHTML = "";
-        document.getElementById("dadiPlayer").innerHTML = "";
-
+function getDice() {
     
-        let risultatiCPU = []
-        for (let i = 0; i < 3; i++) {
-            let numeroCasuale = Math.floor(Math.random() * 6) + 1;
-            risultatiCPU.push(numeroCasuale);
-        }
+    document.getElementById("dadiCPU").innerHTML = "";
+    document.getElementById("dadiPlayer").innerHTML = "";
 
-        let risultatiPlayer = []
-        for (let i = 0; i < 3; i++) {
-            let numeroCasuale = Math.floor(Math.random() * 6) + 1;
-            risultatiPlayer.push(numeroCasuale);
-        }
+    risultatiCPU = []
+    for (let i = 0; i < 3; i++) {
+        let numeroCasuale = Math.floor(Math.random() * 6) + 1;
+        risultatiCPU.push(numeroCasuale);
+    }
+
+    risultatiPlayer = []
+    for (let i = 0; i < 3; i++) {
+        let numeroCasuale = Math.floor(Math.random() * 6) + 1;
+        risultatiPlayer.push(numeroCasuale);
+    }
+
+    risultatiCPU.forEach(function(numero) {
+        let diceImg = document.createElement('img');
+        diceImg.classList.add('dice');
+        diceImg.src = 'img/dice' + numero + '.png';
+        document.getElementById('dadiCPU').appendChild(diceImg);
+    });
+
+    risultatiPlayer.forEach(function(numero) {
+        let diceImg = document.createElement('img');
+        diceImg.classList.add('dice');
+        diceImg.src = 'img/dice' + numero + '.png';
+        document.getElementById('dadiPlayer').appendChild(diceImg);
+    });
+
+}
+
+
+
+function shuffleAnimation() {
+
+    let button = document.getElementById("game_button");
+    button.innerHTML = "In corso...";
+    button.disabled = true;
     
-        risultatiCPU.forEach(function(numero) {
-            let diceImg = document.createElement('img');
-            diceImg.classList.add('dice');
-            diceImg.src = 'img/dice' + numero + '.png';
-            document.getElementById('dadiCPU').appendChild(diceImg);
-        });
+    const animation = setInterval(() => {getDice();}, 75);
+    setTimeout(() => clearInterval(animation), 1500);
+
+    setTimeout(() => rollDice(), 1500);
+    setTimeout(() => {
+        button.disabled = false;
+        button.innerHTML = "Lancia i dadi!";
+    }, 2500);
+
+}
+
+
+
+function rollDice() {
+
+    getDice();
+    setTimeout(() => calculateEnd(), 500);
+
+}
+
+
+
+function calculateEnd() {
     
-        risultatiPlayer.forEach(function(numero) {
-            let diceImg = document.createElement('img');
-            diceImg.classList.add('dice');
-            diceImg.src = 'img/dice' + numero + '.png';
-            document.getElementById('dadiPlayer').appendChild(diceImg);
-        });
+    let punteggioCPU = risultatiCPU.reduce((accumulator, currentValue) => accumulator + currentValue);
+    let punteggioPlayer = risultatiPlayer.reduce((accumulator, currentValue) => accumulator + currentValue);
 
-
-        let punteggioCPU = risultatiCPU.reduce((accumulator, currentValue) => accumulator + currentValue);
-        let punteggioPlayer = risultatiPlayer.reduce((accumulator, currentValue) => accumulator + currentValue);
-
-        if (punteggioPlayer > punteggioCPU) {
-            puntiPlayer ++
-        }
+    if (punteggioPlayer > punteggioCPU) {
+        puntiPlayer ++
+    }
       
-        if (punteggioPlayer < punteggioCPU) {
-            puntiCPU ++
+    if (punteggioPlayer < punteggioCPU) {
+        puntiCPU ++
+    }
+
+
+    document.getElementById("pointsCPU").innerHTML = puntiCPU;
+    document.getElementById("pointsPlayer").innerHTML = puntiPlayer;
+
+    setTimeout(() => {
+        if (puntiCPU == limite) {
+            alert("Vince la CPU!");
+            location.href = "index.html";
         }
+        if (puntiPlayer == limite) {
+            alert("Hai vinto!!");
+            location.href = "index.html";
+        }
+    }, 1000);
 
-        document.getElementById("pointsCPU").innerHTML = puntiCPU;
-        document.getElementById("pointsPlayer").innerHTML = puntiPlayer;
-
-
-    if (puntiCPU == limite) {
-        alert("Vince la CPU!");
-        location.href = "index.html";
-    }
-    if (puntiPlayer == limite) {
-        alert("Hai vinto!!");
-        location.href = "index.html";
-    }
 }
